dcc31b980a916d65deb4ba7b865a9ea34586529f
Tobias Ekman <nilstobiasekman@gmail.com>
2021-03-17 10:53:25 +0100

    step 4

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -5,7 +5,18 @@ public class Calculator {
     public static int add(String numbers) {
 
         int result = 0;
-        String[] numbersArray = numbers.split(",|\n");
+        String delimiter = "";
+
+        if (numbers.startsWith("//")) {
+            String[] newDelim = numbers.split("\n", 2);
+            delimiter = newDelim[0].replaceFirst("//", "");
+            numbers = newDelim[1];
+        }
+        else {
+            delimiter = ",|\n";
+        }
+
+        String[] numbersArray = numbers.split(delimiter);
 
         for (String s : numbersArray) {
             result = result + Integer.parseInt(s);
--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -1,18 +1,32 @@
 package calculator;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class CalculatorTest {
 
-    @Test
-    void getSumFromNumbersInString() {
-        String input = "10,5,5,26\n54";
-        int expectedSum = 100;
+    @ParameterizedTest
+    @MethodSource("provideStringsForGetSumFromNumbersInString")
+    void getSumFromNumbersInString(String input, int expectedSum) {
 
         int actualSum = Calculator.add(input);
 
         assertThat(actualSum).isEqualTo(expectedSum);
     }
+
+    private static Stream<Arguments> provideStringsForGetSumFromNumbersInString() {
+        return Stream.of(
+                Arguments.of("10,5", 15),
+                Arguments.of("50,35,25", 110),
+                Arguments.of("15,5\n10\n20", 50),
+                Arguments.of("//;\n10;30", 40),
+                Arguments.of("//hello\n10hello30hello40", 80)
+        );
+    }
+
 }
